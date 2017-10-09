74759688cbd4db24b2ca1609438ae8a2d03ea235
Andrew Kent <andmkent@iu.edu>
2017-10-09 15:55:16 -0400

    more

--- lang.rkt
+++ lang.rkt
@@ -2,11 +2,12 @@
 
 (require "set-utils.rkt")
 
-(define-type Atom (U Symbol Range Prod Arrow))
+(define-type Tag Symbol)
+
+(define-type Atom (U Tag Range Prod Arrow))
 (define-type Type (U Atom Var Rec Or And (Not Type)))
 
-(define-type Bound (U Integer -inf.0 +inf.0))
-(struct Range ([lower : Bound] [upper : Bound]) #:transparent)
+(struct Range ([lower : Real] [upper : Real]) #:transparent)
 (struct Prod ([l : Type] [r : Type]) #:transparent)
 (struct Arrow ([dom : Type] [rng : Type]) #:transparent)
 (struct Or ([ts : (Setof Type)]) #:transparent)
@@ -25,11 +26,88 @@
 
 (define-type Literal (U Atom (Not Atom)))
 
+
+(struct Disjunct ([clauses : (Listof Conjunct)]))
+
+(define-type Conjunct (U TagConjunct
+                         RangeConjunct
+                         ProdConjunct
+                         ArrowConjunct))
+
+(struct TagConjunct ([pos : (Setof Tag)] [neg : (Setof Tag)]))
+(struct RangeConjunct ([pos : (Setof Range)] [neg : (Setof Range)]))
+(struct ProdConjunct ([pos : (Setof Prod)] [neg : (Setof Prod)]))
+(struct ArrowConjunct ([pos : (Setof Arrow)] [neg : (Setof Arrow)]))
+
+
+(: ->DNF (-> Type Disjunct))
+(define (->DNF t) (error 'todo))
+
 (: subtype? (-> Type Type Boolean))
 (define (subtype? t1 t2)
-  (uninhabited? (And (set t1 (Not t2)))))
+  (uninhabited-disjunct?
+   (->DNF (And (set t1 (Not t2))))))
 
-(: uninhabited? (-> Type Boolean))
-(define (uninhabited? t) (error 'todo))
+(: uninhabited-disjunct? (-> Disjunct Boolean))
+(define (uninhabited-disjunct? d)
+  (andmap uninhabitited-conjunct? (Disjunct-clauses d)))
 
 
+(: uninhabitited-conjunct? (-> Conjunct Boolean))
+(define (uninhabitited-conjunct? c)
+  (cond
+    [(TagConjunct? c)
+     (define pos-tags (TagConjunct-pos c))
+     (define neg-tags (TagConjunct-pos c))
+     (cond
+       ;; tags are necessarily disjoint, so if there are 2
+       ;; or more this type is uninhabited
+       [(> 1 (set-count pos-tags)) #true]
+       ;; is there we know P and ¬P, we have a contradiction
+       ;; and the type is not inhabited
+       [(sets-overlap? pos-tags neg-tags) #true]
+       ;; otherwise it's possible this conjunct is inhabited
+       [else #false])]
+    [(RangeConjunct? c)
+     (define pos-ranges (RangeConjunct-pos c))
+     (define neg-ranges (RangeConjunct-pos c))
+     (define pos (reduce-pos-ranges pos-ranges))
+     (uninhabited-range? (reduce-range-with-negs pos neg-ranges))]
+    [(ProdConjunct? c)
+     (define pos-tags (ProdConjunct-pos c))
+     (define neg-tags (ProdConjunct-pos c))
+     (error 'todo-ProdConjunct)]
+    [(ArrowConjunct? c)
+     (define pos-tags (ArrowConjunct-pos c))
+     (define neg-tags (ArrowConjunct-pos c))
+     (error 'todo-ArrowConjunct)]))
+
+(: reduce-pos-ranges (-> (Setof Range) Range))
+(define (reduce-pos-ranges pos-ranges)
+  (let-values
+      ([(lower upper)
+        (for/fold ([lower : Real -inf.0]
+                   [upper : Real +inf.0])
+                  ([r (in-set pos-ranges)])
+          (values (max lower (Range-lower r))
+                  (min upper (Range-upper r))))])
+    (Range lower upper)))
+
+
+(: uninhabited-range? (-> Range Boolean))
+(define (uninhabited-range? r)
+  (define lower (Range-lower r))
+  (define upper (Range-upper r))
+  (and lower upper (> lower upper)))
+
+(: reduce-range-with-negs (-> Range (Setof Range) Range))
+(define (reduce-range-with-negs pos neg-ranges)
+  (error 'foo))
+
+(: range-subtract (-> Range Range Range))
+(define (range-subtract r1 r2)
+  (define lower (Range-lower r1))
+  (define upper (Range-upper r1))
+  (define neg-lower (Range-lower r2))
+  (define neg-upper (Range-upper r2))
+  (error 'todo))
--- set-utils.rkt
+++ set-utils.rkt
@@ -1,5 +1,7 @@
 #lang typed/racket/base
 
+(require (for-syntax typed/racket/base))
+
 (provide (all-defined-out))
 
 (define-type (Setof T) (Immutable-HashTable T True))
@@ -8,6 +10,8 @@
 (define (set . elems)
   (for/hash : (Setof T) ([elem (in-list elems)])
     (values elem #t)))
+
+(define set-count hash-count)
 
 (: set-add (All (T) (-> (Setof T) T (Setof T))))
 (define (set-add s elem)
@@ -32,3 +36,14 @@
   (for/fold ([s : (Setof T) s])
             ([elem (in-immutable-hash-keys others)])
     (hash-remove s elem)))
+
+(: sets-overlap? (All (T) (-> (Setof T) (Setof T) Boolean)))
+(define (sets-overlap? s1 s2)
+  (cond
+    [(> (set-count s1) (set-count s2)) (sets-overlap? s2 s1)]
+    [else
+     (for/or : Boolean ([elem (in-immutable-hash-keys s1)])
+       (hash-has-key? s2 elem))]))
+
+
+(define-syntax in-set (make-rename-transformer #'in-immutable-hash-keys))
